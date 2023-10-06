82dc58ffcee77c2c58a26113f078d129630ffcb6
Ghulam Mohiuddin <45088158+optimumprogrammer@users.noreply.github.com>
2023-10-06 06:13:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ TERMUXURLOPENER_CONFIG_FOLDER="${HOME}/bin/"
 
 echo "Hi, This script sets up an environment to download various videos from various apps"
 sleep 1
-echo -e "\n\n${BLUE}Requirements :"
+echo -e "\n${BLUE}Requirements :"
 echo -e "${NC}    1. Allow storage access to Termux"
 echo -e "${NC}    2. A working internet connection.\n\n"
 read -p "When you are ready just press enter:"
@@ -18,15 +18,15 @@ read -p "When you are ready just press enter:"
 termux-setup-storage
 sleep 2
 pkg update
-pkg -y install python ffmpeg
+pkg install python ffmpeg
 pip install yt-dlp
 mkdir -p $YOUTUBEDL_OUTPUT_FOLDER
 mkdir -p $YOUTUBEDL_CONFIG_FOLDER
 mkdir -p $TERMUXURLOPENER_CONFIG_FOLDER
 # Download setup & config file
-curl -L https://raw.githubusercontent.com/OzakIOne/termux-youtube-dl/v2.0.1/.ytdl.conf >"${YOUTUBEDL_CONFIG_FOLDER}/config"
-curl -L https://raw.githubusercontent.com/OzakIOne/termux-youtube-dl/v2.0.1/sponsorblock.conf >"${YOUTUBEDL_CONFIG_FOLDER}/sponsorblock.conf"
-curl -L https://raw.githubusercontent.com/OzakIOne/termux-youtube-dl/v2.0.1/share2youtubedl.sh >"${TERMUXURLOPENER_CONFIG_FOLDER}/termux-url-opener"
+curl -L https://raw.githubusercontent.com/optimumprogrammer/termux-youtube-dl/master/.ytdl.conf >"${YOUTUBEDL_CONFIG_FOLDER}/config"
+curl -L https://raw.githubusercontent.com/optimumprogrammer/termux-youtube-dl/master/sponsorblock.conf >"${YOUTUBEDL_CONFIG_FOLDER}/sponsorblock.conf"
+curl -L https://raw.githubusercontent.com/optimumprogrammer/termux-youtube-dl/master/share2youtubedl.sh >"${TERMUXURLOPENER_CONFIG_FOLDER}/termux-url-opener"
 
 echo -e "${BLUE}Congratulations!!! Your setup is complete.\n\n"
 read -p "When you are ready just press enter"
